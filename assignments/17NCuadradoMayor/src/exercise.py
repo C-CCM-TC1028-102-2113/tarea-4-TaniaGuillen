0b4f536148b8b1da9e01e8d09fdae7d32d1a1771
TaniaGuillen <89371496+TaniaGuillen@users.noreply.github.com>
2021-09-07 10:21:05 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -2,6 +2,18 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    x=0
+raiz= 0
+z= 0
+a=0
+import math
+
+x = int(input('Escribe un numero: '))
+raiz = (math.sqrt(x))
+z = raiz+1
+a = z*z
+if (a>x):
+    print(z)
     pass
 
 if __name__=='__main__':
