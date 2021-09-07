c18a67f3cc5f661bd8309b133acb2e408c925a1b
TaniaGuillen <89371496+TaniaGuillen@users.noreply.github.com>
2021-09-07 10:30:28 -0500

    Update exercise.py

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,6 +1,13 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+n= int(input('Escribe la altura del triangulo: '))
+a=n
+asterisco=0
+for i in range (1, n+1, 1):
+    asterisco= '*' *i
+    print(asterisco)
+    a= a-1
     pass
 
 
