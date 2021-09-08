3877e3361096f9bf80fe706cca5aa5ea09348589
TaniaGuillen <89371496+TaniaGuillen@users.noreply.github.com>
2021-09-08 13:53:10 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,6 +1,13 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    n = int(input('Numero: '))
+    a, b = 0, 1
+    for i in range (1, n):
+        c = a + b
+        a = b
+        b = c
+    print (b)
     pass
 
 if __name__=='__main__':
